28a2c0cd035678a6449d1c31b97052681d04202e
Rémi Robichet <remi.robichet@edtake.com>
2021-02-24 13:44:30 +0100

    :tada: Init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <button>ffffffnoise</button>
+  </div>
+</template>
+
+<script>
+import * as Tone from 'tone'
+export default {
+  // https://tonejs.github.io/docs/r13/Noise
+  mounted () {
+    const noise = new Tone.Noise('pink').start()
+    const autoFilter = new Tone.AutoFilter({
+      frequency: '8m',
+      min: 800,
+      max: 15000
+    }).connect(Tone.Master)
+
+    // connect the noise
+    noise.connect(autoFilter)
+    // start the autofilter LFO
+    autoFilter.start()
+  }
+}
+</script>
+
+<style>
+</style>
